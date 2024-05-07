f22098c9dd267ad938375f2c56e8d43b4a530f7e
otsukatatsuya <93520179+otsukatatsuya@users.noreply.github.com>
2024-05-07 11:29:07 +0000

    不要な差分削除

--- src/stories/Button.jsx
+++ src/stories/Button.jsx
@@ -3,15 +3,17 @@ import PropTypes from "prop-types";
 import "./button.css";
 
 /**
- * Primary UI component for user interaction
+ * ボタンコンポーネント
  */
 export const Button = ({ primary, backgroundColor, size, label, ...props }) => {
+  //Primaryかどうかに応じて、modeを変更する
   const mode = primary
     ? "storybook-button--primary"
     : "storybook-button--secondary";
   return (
     <button
       type="button"
+      //ベースのclassNameとpropsを適用する
       className={["storybook-button", `storybook-button--${size}`, mode].join(
         " ",
       )}
--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -2,12 +2,10 @@ import { Button } from "./Button";
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
 export default {
+  //階層構造を表現できる
   title: "Example/Button",
+  //対象のComponentを定義
   component: Button,
-  tags: ["autodocs"],
-  argTypes: {
-    backgroundColor: { control: "color" },
-  },
 };
 
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
